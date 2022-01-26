557ad169dd970b23f409f890605df30ad1ce175e
Ridwan Arifandi <orangerdigiart@gmail.com>
2022-01-26 21:01:09 +0700

    first initialization

---
+++ init/01.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `wptests`;
+GRANT ALL ON `wptests`.* TO 'wordpress'@'%';
---
+++ wordpress/wp-content/plugins/alodokter-paf-assignment/alodokter-paf-assignment.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Alodokter - Principal Architect Framework assignment
+Plugin URI: https://ridwan-arifandi.com
+Description: Assignment for Principal Architect Framework position.
+Version: 1.0.0
+Author: Ridwan Arifandi
+Author URI: https://ridwan-arifandi.com
+License: GPLv2 or later
+Text Domain: alo
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) :
+	die;
+endif;
